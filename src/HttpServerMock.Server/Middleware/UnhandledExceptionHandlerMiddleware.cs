5b7e3949aaf2d883566c7d1f86f1de5e91ac9659
Solovyov D <solovyovd@gmail.com>
2020-09-09 23:27:25 +0300

    Update error middleware

--- src/HttpServerMock.Server/Middleware/UnhandledExceptionHandlerMiddleware.cs
+++ src/HttpServerMock.Server/Middleware/UnhandledExceptionHandlerMiddleware.cs
@@ -1,9 +1,9 @@
-﻿using System.Net;
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System.Net;
 using System.Net.Mime;
 using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
 
 namespace HttpServerMock.Server.Middleware
 {
@@ -20,15 +20,17 @@ namespace HttpServerMock.Server.Middleware
         {
             try
             {
-                //httpContext.Request.EnableBuffering();
-
                 await _next(httpContext);
             }
             catch
             {
                 httpContext.Response.ContentType = MediaTypeNames.Application.Json;
                 httpContext.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
-                await httpContext.Response.WriteAsync("API failed on request processing!", CancellationToken.None);
+
+                if (!httpContext.Response.HasStarted)
+                {
+                    await httpContext.Response.WriteAsync("API failed on request processing!", CancellationToken.None);
+                }
             }
         }
     }
